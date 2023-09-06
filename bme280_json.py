91a0500fa6069bd8c4ec8548d385706cf8235aab
fourstops <fourstops@gmail.com>
2023-09-06 12:09:43 -0400

    Added Loop

--- bme280_json.py
+++ bme280_json.py
@@ -10,31 +10,35 @@ bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 
-data = {
-    "feed_url": "https://tidbyt-json-display.s3.eu-west-1.amazonaws.com/example.json",
-    "title_image": "https://tidbyt-json-display.s3.eu-west-1.amazonaws.com/heart.png",
-    "title_text": "BME280",
-    "data": [
-        {
-            "title": "Temperature",
-            "value": bme280.temperature,
-            "color": "FF0000"
-        },
-        {
-            "title": "Pressure",
-            "value": bme280.pressure,
-            "color": "00FF00"
-        },
-        {
-            "title": "Humidity",
-            "value": bme280.humidity,
-            "color": "0000FF"
-        }
-    ]
-}
-
-# Convert the dictionary to a JSON string
-json_string = json.dumps(data, indent=4)
-
-# Print the JSON string
-print(json_string)
+while True:
+    temperature = "{:.2f}".format(bme280.temperature)
+    pressure = "{:.2f}".format(bme280.pressure)
+    humidity = "{:.2f}".format(bme280.humidity)
+    
+    data = {
+        "feed_url": "http://192.168.0.89/bme280.json",
+        "title_image": "https://tidbyt-json-display.s3.eu-west-1.amazonaws.com/heart.png",
+        "title_text": "BME280",
+        "data": [
+            {
+                "title": "Temperature",
+                "value": temperature,
+                "color": "FF0000"
+            },
+            {
+                "title": "Pressure",
+                "value": pressure,
+                "color": "00FF00"
+            },
+            {
+                "title": "Humidity",
+                "value": humidity,
+                "color": "0000FF"
+            }
+        ]
+    }
+    
+    # Convert the dictionary to a JSON string
+    json_string = json.dumps(data, indent=4)
+    print(json_string)
+    time.sleep(15)
